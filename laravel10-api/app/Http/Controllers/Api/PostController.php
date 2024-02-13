c01d0f62f67075b126799861d8f0cf195aef14af
shifakurnialatifa <jasjusmangomango@gmail.com>
2024-02-13 11:47:51 +0700

    add file api & PostController

--- laravel10-api/app/Http/Controllers/Api/PostController.php
+++ laravel10-api/app/Http/Controllers/Api/PostController.php
@@ -8,24 +8,28 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Http\Resources\PostResource;
-
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
     /**
      * index
-     * 
+     *
      * @return void
      */
     public function index()
     {
+        //get all posts
         $posts = Post::latest()->paginate(5);
+
+        //return collection of posts as a resource
         return new PostResource(true, 'List Data Posts', $posts);
     }
     /**
      * store
-     * 
+     *
      * @param mixed $request
      * @return void
      */
@@ -49,6 +53,86 @@ class PostController extends Controller
             'title'=> $request->title,
             'content' => $request->content,
         ]);
-        return new PostResource(true, 'Data Post Brhasil Ditambahkan!', $post);
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
+    }
+
+    /**
+     * show
+     *
+     * @param mixed $post
+     * @return void
+     */
+    public function show($id)
+    {
+        //find post by ID
+        $post = Post::find($id);
+
+        //return single post as a resource
+        return new PostResource(true, 'Detail Data Post!', $post);
+    }
+
+
+    //codingan ini digunakan untuk ngupdate data yang kita sudah masukan
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(),[
+            'title' =>'required | min:10',
+            'content' =>'required | min:10',
+        ]);
+
+        //check validator
+        if($validator->fails()){
+            return Response()->json($validator->errors(), 422);
+        }
+
+        //get ID
+        $post = Post::find($id);
+
+        //check image if no empty
+        if($request->hasFile('image')) {
+
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/posts/' . basename($post->image));
+
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        }else{
+
+            //update post without image
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        }
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
+    }
+
+    /**
+     * destroy
+     *
+     * @param mixed $post
+     * @return void
+     */
+
+
+     //codingan ini kegunaannnya untuk mendelete data yang kita inginkan
+    public function destroy($id)
+    {
+        //find post by ID
+        $post = Post::find($id);
+
+        //delete image
+        Storage::delete('public/posts/' . basename($post->image));
+
+        $post->delete();
+        return new PostResource(true, 'Data Berhasil Dihapus!', $post);
     }
 }
--- laravel10-api/routes/api.php
+++ laravel10-api/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
+
 
